197836647d7e5bf80af4657c9ef37d33f0aac566
Renato Groffe <renato.groffe@yahoo.com.br>
2021-11-26 19:27:06 -0300

    Novos ajustes

--- Groffe.Alturas/ConversorAlturas.cs
+++ Groffe.Alturas/ConversorAlturas.cs
@@ -7,8 +7,12 @@ public class ConversorAlturas
     /// </summary>
     /// <param name="alturaPes">Altura em pes</param>
     /// <returns></returns>
+    /// <exception cref="ArgumentException">Excecao lancada para uma altura em pes negativa ou igual a zero.</exception>
     public static double PesParaMetros(double alturaPes)
     {
+        if (alturaPes <= 0)
+            throw new ArgumentException("A altura em pes deve ser um valor maior do que zero!");
+
         return Math.Round(alturaPes * 0.3048, 4);
     }
 }
